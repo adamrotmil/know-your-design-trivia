8ad83948484145495fa9eeb1d12712c5ef781e31
adamrotmil <adam.rotmil@gmail.com>
2015-08-21 19:43:01 -0400

    new logic

--- js/app.js
+++ js/app.js
@@ -1,25 +1,55 @@
-function globalSwap() { // reset the animation on the text
+function typeToggle() { // reset the animation on the text
 	var e = document.getElementById("question")
 	e.classList.remove("typing")
 	e.offsetWidth = e.offsetWidth
 	e.classList.add("typing");
 }
 
-score = 0 // start with no score
+// data for answers
+quiz = {};
+quiz.answer = {
+	"paul rand": true
+};
 
-// pick a random number to later randomize question order
-var myNumber = Math.floor((Math.random() * 20) + 1);
+
+
+// listener for button click
+$(document).on('mousedown', '.button-one', function() {
+	var guess = this.text().toLowerCase();
+	console.log(guess);
+	if ((quiz.answer[guess]) === true) {
+		console.log("evaluated to true boolean")
+	}
+});
+
+
+
+$(document).on('mousedown', '.button-two', function() {
+
+});
+
+$(document).on('mousedown', '.button-three', function() {
+
+});
+
+$(document).on('mousedown', '.button-four', function() {
+
+});
+
+
 
 // practice swap the big image and other stuff using the object key values
 function imageUseObject() {
 	$('.big-photo').css('background-image', 'url(' + question01.image + ')');
 	$('h1.question').text(question01.query);
-	globalSwap() // reset the animation on text
+	typeToggle(); // reset the animation on text
 	$('div.button-one > p.button-text').text(question01.answer01);
 	$('div.button-two > p.button-text').text(question01.answer02);
 	$('div.button-three > p.button-text').text(question01.answer03);
 	$('div.button-four > p.button-text').text(question01.answer04);
 }
+
+
 
 // define objects for quiz contents
 var question01 = {
@@ -34,60 +64,12 @@ var question01 = {
 	image: "\"./images/paula.jpg\"",
 }
 
-// answer key
-switch (myNumber) {
-	case 1:
-		rightAnswer = "01"
-		break;
-	default:
-		rightAnswer = "01"
-}
 
-// listener for button click
-$(document).on('mousedown', '.button-one', function() {
-	// console.log("mousedown for button one")
-	yourAnswer = "01"
-	if (yourAnswer == rightAnswer) {
-		console.log("you got it!")
-		score = score++;
-		imageUseObject();
-	} else {
-		console.log("not quite!")
-	}
-});
 
-$(document).on('mousedown', '.button-two', function() {
-	// console.log("mousedown for button two")
-	yourAnswer = "02"
-	if (yourAnswer == rightAnswer) {
-		console.log("you got it!")
-		score = score++;
-	} else {
-		console.log("not quite!")
-	}
-});
+score = 0 // start with no score
 
-$(document).on('mousedown', '.button-three', function() {
-	// console.log("mousedown for button three")
-	yourAnswer = "03"
-	if (yourAnswer == rightAnswer) {
-		console.log("you got it!")
-		score = score++;
-	} else {
-		console.log("not quite!")
-	}
-});
-
-$(document).on('mousedown', '.button-four', function() {
-	// console.log("mousedown for button four")
-	yourAnswer = "04"
-	if (yourAnswer == rightAnswer) {
-		console.log("you got it!")
-		score = score++;
-	} else {
-		console.log("not quite!")
-	}
-});
+// pick a random number to later randomize question order
+var myNumber = Math.floor((Math.random() * 20) + 1);
 
 
 
