b414ad0b3f58a4386336afa6155d22e709bcf742
adamrotmil <adam.rotmil@gmail.com>
2015-08-22 15:33:17 -0400

    advancing question behavior

--- js/app.js
+++ js/app.js
@@ -1,36 +1,70 @@
-function typeToggle() { // reset the animation on the text
+var score = 0 // start with no score
+var questionFlag = 1 // which question are you on
+var myNumber = Math.floor((Math.random() * 20) + 1); // pick a random number
+// reset the animation on the text
+function typeToggle() {
 	var e = document.getElementById("question")
 	e.classList.remove("typing")
 	e.offsetWidth = e.offsetWidth
 	e.classList.add("typing");
 }
-
-function showError() { // shake the screen
+// shake the screen
+function showError() {
 	var e = document.getElementById("frame")
 	e.classList.remove("shake")
 	e.offsetWidth = e.offsetWidth
 	e.classList.add("shake");
 }
-
-// data for answers
+// questions and answers
 quiz = {};
-quiz.answer = {
-	"paul rand": true
-};
+switchQuestion();
 
+function switchQuestion() {
+	switch (questionFlag) {
+		case 1:
+			console.log("it was case 1 in switch");
+			quiz.answer = {
+				"paul rand": true
+			};
+			break;
+		case 2:
+			console.log("it was case 2 in switch");
+			quiz.answer = {
+				"paula scher": true
+			};
+			quizdata = {
+				image: "./images/paula.jpg",
+				headline: "She drew the logo for Citi on a napkin at lunch",
+				choices: ["April Greiman", "Yuko Takagi", "Paula Scher", "Lucia Collela"]
+			};
+			break;
+	};
+}
 
-
-// listener for button click
+// click listener and behaviors
 $(document).on('mousedown', '.button', function() {
 	var guess = ($(this).text().toLowerCase());
 	if ((quiz.answer[guess]) === true) {
 		console.log("evaluated to true boolean")
 			// correct answer behavior
+		score = (score + 1);
+		questionFlag = (questionFlag + 1);
+		console.log("Current score is " + score);
 		$(".overlay").fadeIn(1000);
 		$("a.close").click(function() {
+			// go to next question behavior
 			$(".overlay").fadeOut(1000);
+			switchQuestion();
+			console.log("you are now on question " + questionFlag);
+			console.log(quiz.answer);
+			$('.big-photo').css('background-image', 'url(' + quizdata.image + ')');
+			$('h1.question').text(quizdata.headline);
+			typeToggle() //reset headline animation
+			$('#choice-one').text(quizdata.choices[0]);
+			$('#choice-two').text(quizdata.choices[1]);
+			$('#choice-three').text(quizdata.choices[2]);
+			$('#choice-four').text(quizdata.choices[3]);
 		});
-
 	} else {
 		console.log("false")
 			// wrong answer behavior
@@ -38,65 +72,9 @@ $(document).on('mousedown', '.button', function() {
 	}
 });
 
-
-
-$(document).on('mousedown', '.button-two', function() {
-
-});
-
-$(document).on('mousedown', '.button-three', function() {
-
-});
-
-$(document).on('mousedown', '.button-four', function() {
-
-});
-
-
-
-// practice swap the big image and other stuff using the object key values
-function imageUseObject() {
-	$('.big-photo').css('background-image', 'url(' + question01.image + ')');
-	$('h1.question').text(question01.query);
-	typeToggle(); // reset the animation on text
-	$('div.button-one > p.button-text').text(question01.answer01);
-	$('div.button-two > p.button-text').text(question01.answer02);
-	$('div.button-three > p.button-text').text(question01.answer03);
-	$('div.button-four > p.button-text').text(question01.answer04);
-}
-
-
-
-// define objects for quiz contents
-var question01 = {
-	number: "01",
-	query: "She drew the logo for Citi on a napkin at lunch",
-	answer: "Paula Scher",
-	agency: "Pentagram",
-	answer01: "April Greiman",
-	answer02: "Lella Vignelli",
-	answer03: "Paula Scher",
-	answer04: "Yuko Takagi",
-	image: "\"./images/paula.jpg\"",
-}
-
-
-
-score = 0 // start with no score
-
-// pick a random number to later randomize question order
-var myNumber = Math.floor((Math.random() * 20) + 1);
-
-
-
-// *** control flow
-// randomize question order
-// ask question if new question
-// evaluate input
-// display feedback (modal?)
-// flag question as stale
-// save score
-// ask fresh question at random by pulling from objects
-// eval any questions left?
-// tally up score
-// feedback about score
+// *** to do list
+// use random number to select question
+// avoid repeat questions
+// go to next question behavior
+// tally score at the end behavior
+// maybe score is number of guesses relative to correct answers (accuracy)
